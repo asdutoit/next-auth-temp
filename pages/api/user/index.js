1dd632aa97ca25a3329983f2000133d612a8c8e0
asdutoit@gmail.com <asdutoit@gmail.com>
2021-08-11 07:53:56 +0200

    removed the api/users folder and updated favourites

--- pages/api/user/index.js
+++
@@ -1,5 +0,0 @@
-import { connectToDatabase } from '../../../../utils/mongodb';
-import { getSession } from 'next-auth/client';
-const { ObjectId } = require('mongodb');
-
-export default async function handle(req, res) {}
