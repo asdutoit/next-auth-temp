58c61cd809497760e2a1229dee41ffecb6fe1991
asdutoit@gmail.com <asdutoit@gmail.com>
2021-09-20 11:32:37 +0200

    some additional improvements

--- components/Property/PhotoSlider.js
+++ components/Property/PhotoSlider.js
@@ -127,9 +127,6 @@ function PhotoSlider({ photos, rounded = false, visible }) {
                         height={200}
                         quality={75}
                         key={i}
-                        // blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                        //     shimmer(20, 20)
-                        // )}`}
                         loading="lazy"
                         className={`object-cover ${
                             rounded ? 'rounded-lg' : ''
@@ -142,22 +139,3 @@ function PhotoSlider({ photos, rounded = false, visible }) {
 }
 
 export default PhotoSlider;
-
-// const shimmer = (w, h) => `
-// <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-//   <defs>
-//     <linearGradient id="g">
-//       <stop stop-color="#333" offset="20%" />
-//       <stop stop-color="#222" offset="50%" />
-//       <stop stop-color="#333" offset="70%" />
-//     </linearGradient>
-//   </defs>
-//   <rect width="${w}" height="${h}" fill="#333" />
-//   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-//   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-// </svg>`;
-
-// const toBase64 = (str) =>
-//     typeof window === 'undefined'
-//         ? Buffer.from(str).toString('base64')
-//         : window.btoa(str);
